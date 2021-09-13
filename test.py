1df615be500df58b9fcc05544a3288d7f97a3ce0
syoupheng <do.syoupheng@gmail.com>
2021-09-13 16:58:39 +0200

    finalized API

--- test.py
+++ test.py
@@ -1,11 +1,31 @@
 import requests
 
+data = [{"likes":78, "name":"Joe", "views":80000},
+        {"likes":10, "name":"How to make rest API", "views":100000},
+        {"likes":35, "name":"Tim", "views":20000}]
+
 BASE = "http://127.0.0.1:5000/"
 
-#response = requests.put(BASE + "video/1", {"likes":10, "name":"Tim", "views":100000})
-#print(response.json())
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
 
-#input()
+input()
 
-response = requests.get(BASE + "video/189")
+response = requests.patch(BASE + "video/2", {"name": "Rhoy", "likes": 150})
 print(response.json())
+
+input()
+
+response = requests.delete(BASE + "video/2")
+print(response)
+
+input()
+
+response = requests.get(BASE + "video/2")
+print(response.json())
+
+input()
+
+response = requests.delete(BASE + "video/2")
+print(response)
